27db0a042a45f2c1d6da00e62ba14fa98f8e78c2
QDNBD <1726224208@qq.com>
2020-04-17 18:31:50 +0800

    提交代码

---
+++ 20191117--L/src/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+    public static void main(String[] args) {
+        DoubleList doubleList = new DoubleList();
+        doubleList.AddFirst(3);
+        doubleList.AddFirst(2);
+        doubleList.AddFirst(1);
+        doubleList.AddLast(4);
+        doubleList.AddLast(5);
+        doubleList.AddLast(6);
+        doubleList.display();
+    }
+}
