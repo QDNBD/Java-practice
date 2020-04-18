c87d1774e859f1e62b0ce8b0bc54a3e41ebd1f30
QDNBD <1726224208@qq.com>
2020-04-18 15:19:17 +0800

    提交代码

---
+++ 20191124/src/TestDome.java
@@ -0,0 +1,34 @@
+/*
+
+class Student implements Comparable<Student> {
+    public String name;
+    public int age;
+    public double soue;
+
+    public Student(String name, int age, double score) {
+        this.name = name;
+        this.age = age;
+        this.score = score;
+    }
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", soue=" + soue +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        return this.name.compareTo(o.name);
+        //return (int)(this.score-o.score);
+        //return this.age - o.age;//从小到大
+        //return 0;
+    }
+
+}
+public class TestDome {
+
+}
+*/
