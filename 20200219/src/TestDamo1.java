5fe1abeb12ca09053697f961b809b192f12e8c27
QDNBD <1726224208@qq.com>
2020-07-20 21:35:51 +0800

    提交代码

---
+++ 20200219/src/TestDamo1.java
@@ -0,0 +1,23 @@
+
+
+public class TestDamo1 {
+
+
+    public static void main(String[] args) {
+
+        TestBinaryTree testBinaryTree = new TestBinaryTree();
+
+        testBinaryTree.insert(19);
+        testBinaryTree.insert(9);
+        testBinaryTree.insert(3);
+        testBinaryTree.insert(10);
+        testBinaryTree.insert(14);
+        testBinaryTree.insert(24);
+
+        testBinaryTree.inorder(testBinaryTree.root);
+        System.out.println();
+        testBinaryTree.preOrder(testBinaryTree.root);
+
+
+    }
+}
