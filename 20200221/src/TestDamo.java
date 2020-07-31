21078f2b1e9956f259310a799b150bfb8c7fac57
QDNBD <1726224208@qq.com>
2020-07-31 22:27:07 +0800

    提交代码

---
+++ 20200221/src/TestDamo.java
@@ -0,0 +1,22 @@
+
+
+
+public class TestDamo {
+
+
+    public static void main(String[] args) {
+
+        HashBucket hashBucket = new HashBucket();
+        hashBucket.put(1,34);
+        hashBucket.put(17,24);
+        hashBucket.put(3,9);
+        hashBucket.put(7,14);
+        hashBucket.put(2,66);
+        hashBucket.put(5,45);
+
+        System.out.println("fesffd");
+
+
+
+    }
+}
