a90191a120a72a7c99ee508f128e29ccaf39e43c
QDNBD <1726224208@qq.com>
2020-08-03 22:03:39 +0800

    提交代码

---
+++ 20200226_se/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        prinyResult();
+    }
+
+    private static void prinyResult() {
+        for (int i = 0; i < 10;)
+    }
+}
