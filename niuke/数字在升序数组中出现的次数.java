7c55e18e0e9c3ffb0e84812091d2fd36791ce96c
QDNBD <1726224208@qq.com>
2020-11-15 21:52:44 +0800

    提交代码

---
+++ niuke/数字在升序数组中出现的次数.java
@@ -0,0 +1,42 @@
+public class Solution {
+    public int GetNumberOfK(int [] array , int k) {
+//         if(array.length == 0 || array == null) {
+//             return 0;
+//         }
+//        int left = 0;
+//         int right = array.length-1;
+//         while(array[left] < k  && left < right) {
+//             left++;
+//         }
+//         while(array[right] > k &&  right > left) {
+//             right--;
+//         }
+//         if(array[left] != k) {
+//             return 0;
+//         }
+//         if(array[left] == k && array[right] != k) {
+//             return 1;
+//         }
+//         return (right -left) >= 0 ? (right -left + 1) : 0;
+        
+//         if(array.length == 0 || array == null){
+//             return 0;
+//         }
+//         int sum = 0;
+//         for(int i = 0;i < array.length;i++) {
+//             if(array[i] == k) {
+//                 sum++;
+//             }
+//         }
+//         return sum;
+        
+       
+        
+        
+        
+        
+        
+        
+        
+    }
+}
