d8489d6001773d44f5c768e1d857782ae173709e
QDNBD <1726224208@qq.com>
2021-01-02 22:30:46 +0800

    提交代码

---
+++ leetcode/二叉树中的最大路径和.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int ret = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        getMax(root);
+        return ret;
+    }
+    public int getMax(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        int leftSum = Math.max(0,getMax(root.left));
+        int rightSum = Math.max(0,getMax(root.right));
+        ret = Math.max(ret, leftSum + rightSum + root.val);
+        return Math.max(leftSum,rightSum) + root.val;
+    }
+}
