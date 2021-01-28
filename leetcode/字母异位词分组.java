0eec10e59a40f1c5e42420d972bd445d18787754
QDNBD <1726224208@qq.com>
2021-01-28 20:51:39 +0800

    提交代码

---
+++ leetcode/字母异位词分组.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,ArrayList<String>> map = new HashMap<>();
+        for(String s : strs) {
+            char[] ch = s.toCharArray();
+            Arrays.sort(ch);
+            String key = String.valueOf(ch);
+            if(!map.containsKey(key)) {
+                map.put(key,new ArrayList<String>());
+            }
+            map.get(key).add(s);
+        }
+        return new ArrayList(map.values());
+    }
+}
