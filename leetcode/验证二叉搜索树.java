4a38fe893cb7c0a1393eb76790b6fb9aaf8f696c
QDNBD <1726224208@qq.com>
2021-02-09 12:06:40 +0800

    提交代码

---
+++ leetcode/验证二叉搜索树.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    long cur = Long.MIN_VALUE;
+
+    public boolean isValidBST(TreeNode root) {
+        if(root == null) {
+            return true;
+        }
+        boolean l = isValidBST(root.left);
+        if(cur >= root.val) {
+            return false;
+        }
+        cur = root.val;
+        boolean r = isValidBST(root.right);
+        return l && r;
+    }
+}
