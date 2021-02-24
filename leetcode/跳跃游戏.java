a5c197ebcd8a8fb6d3c153981b33dd0f847a17c2
QDNBD <1726224208@qq.com>
2021-02-24 19:20:46 +0800

    提交代码

---
+++ leetcode/跳跃游戏.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int mostLen = 0;
+        for(int i = 0;i < nums.length;i++) {
+            if(i <= mostLen) {
+                mostLen = Math.max(nums[i] + i,mostLen); 
+                if(mostLen >= nums.length - 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
