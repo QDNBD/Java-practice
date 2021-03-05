c02f47d3abc0d282a3ee7a2732661a69008fe748
QDNBD <1726224208@qq.com>
2021-03-05 23:22:41 +0800

    提交代码

---
+++ leetcode/二叉树的层序遍历.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()) {
+            int count = queue.size();
+            List<Integer> list = new ArrayList<>();
+            while(count > 0) {
+                TreeNode cur = queue.poll();
+                list.add(cur.val);
+                if(cur.left != null) {
+                    queue.add(cur.left);
+                }
+                if(cur.right != null) {
+                    queue.add(cur.right);
+                }
+                count--;
+            }
+            res.add(list);
+        }
+        return res;
+    }
+}
