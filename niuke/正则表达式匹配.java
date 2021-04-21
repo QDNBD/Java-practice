653f3b63918e4db0a900777656e4e6835a7f582e
QDNBD <1726224208@qq.com>
2021-04-21 21:04:23 +0800

    提交代码

--- niuke/正则表达式匹配.java
+++ niuke/正则表达式匹配.java
@@ -35,4 +35,45 @@ public class Solution {
         }
         return false;
     }
+	
+	
+	
+	
+	
+	import java.util.*;
+
+
+public class Solution {
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     * 
+     * @param str string字符串 
+     * @param pattern string字符串 
+     * @return bool布尔型
+     */
+    public boolean match (String str, String pattern) {
+        // write code here
+         char[] t = str.toCharArray();
+        char[] p = pattern.toCharArray();
+        return isMatchHelper(t, 0, p, 0);
+    }
+     private boolean isMatchHelper(char[] t, int index, char[] p, int pIndex) {
+        //匹配完成
+        if (index == t.length && pIndex == p.length) {
+            return true;
+        }
+        //未匹配完全
+        if (index != t.length && pIndex == p.length) {
+            return false;
+        }
+        //当前字符匹配判断
+        boolean matchSuc = index < t.length && (p[pIndex] == t[index] || p[pIndex] == '.');
+        if (p.length - pIndex >= 2 && p[pIndex + 1] == '*') {
+            //匹配0次或多次
+            return isMatchHelper(t, index, p, pIndex + 2) || (matchSuc && isMatchHelper(t, index + 1, p, pIndex));
+        }
+        return matchSuc && isMatchHelper(t, index + 1, p, pIndex + 1);
+    }
+}
 }
