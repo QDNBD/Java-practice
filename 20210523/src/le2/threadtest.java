7ce4f6db9fbabd604f46250cd5489fbf3f402e99
QDNBD <1726224208@qq.com>
2021-07-17 23:50:27 +0800

    提交代码

---
+++ 20210523/src/le2/threadtest.java
@@ -0,0 +1,22 @@
+package le2;
+
+public class threadtest {
+    public static void main(String[] args) {
+        for(int i = 0; i < 20; i++) {
+            final int n = i;
+            Thread t = new Thread(new Runnable() {
+                @Override
+                public void run() {//内部类使用外部的变量，必须是final修饰
+                    try {
+                        Thread.sleep(3000);
+                        System.out.println(n);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            t.start();
+        }
+        System.out.println("ok");
+    }
+}
