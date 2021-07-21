c04301d6ea1e99171212f59b1b1b106e9d75ee82
QDNBD <1726224208@qq.com>
2021-07-21 23:53:31 +0800

    提交代码

--- leetcode/分割等和子集.java
+++ leetcode/分割等和子集.java
@@ -23,3 +23,32 @@ class Solution {
         return false;
     }
 }
+
+
+class Solution {
+    public boolean canPartition(int[] nums) {
+        if(nums.length <= 1) {
+            return false;
+        }
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        if(sum % 2 != 0) {
+            return false;
+        }
+        int[] dp = new int[sum / 2 + 1];
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = sum / 2; j >= nums[i]; j--) {
+                dp[j] = Math.max(dp[j], dp[j - nums[i]] + nums[i]);
+            }
+            if(dp[sum /2] == (sum / 2)) {
+                return true;
+            }
+        }
+        if(dp[sum /2] == (sum / 2)) {
+                return true;
+        }
+        return false;
+    }
+}
