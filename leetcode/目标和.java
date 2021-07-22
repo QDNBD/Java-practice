b634d4eb5be643123c69dc8edc893d4a7d8e1a68
QDNBD <1726224208@qq.com>
2021-07-22 23:48:43 +0800

    提交代码

--- leetcode/目标和.java
+++ leetcode/目标和.java
@@ -1,17 +1,43 @@
 class Solution {
     int res = 0;
-    public int findTargetSumWays(int[] nums, int S) {
-        getTarget(nums,S,0);
+
+    public int findTargetSumWays(int[] nums, int target) {
+        Target(nums, target, 0);
         return res;
     }
-    private void getTarget(int[] nums,int S,int index) {
+
+    public void Target(int[] nums, int target, int index) {
         if(index == nums.length) {
-            if(S == 0) {
+            if(target == 0) {
                 res++;
             }
-            return ;
+            return;
         }
-        getTarget(nums,S - nums[index],index + 1);
-        getTarget(nums,S + nums[index],index + 1);
+        Target(nums, target - nums[index], index + 1);
+        Target(nums, target + nums[index], index + 1);
+    }
+}
+
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        if(target > sum) {
+            return 0;
+        }
+        if((sum + target) % 2 == 1) {
+            return 0;
+        }
+        int bigsize = (sum + target) / 2;
+        int[] dp = new int[bigsize + 1];
+        dp[0] = 1;
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = bigsize; j >= nums[i]; j--) {
+                dp[j] += dp[j - nums[i]];
+            }
+        }
+        return dp[bigsize];
     }
 }
