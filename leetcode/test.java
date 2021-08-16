c628a0ad59f1c84787451bb404e976b805b3dded
QDNBD <1726224208@qq.com>
2021-08-16 22:26:50 +0800

    commit code

--- leetcode/test.java
+++ leetcode/test.java
@@ -1,1 +1,21 @@
-测试
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length() <= 1) {
+            return false;
+        }
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == '(') {
+                stack.add(')');
+            }else if(s.charAt(i) == '[') {
+                stack.add(']');
+            }else if(s.charAt(i) == '{'){
+                stack.add('}');
+            }else if(stack.isEmpty() || stack.pop() != s.charAt(i)) {
+                return false;
+            }
+                
+        }
+        return stack.isEmpty();
+    }
+}
